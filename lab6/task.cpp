cb013e49e809dc7b9db668bb51a692015548f765
svetlana <svetlana.r.fisher@gmail.com>
2025-05-01 19:20:37 +0700

    fix

--- lab6/task.cpp
+++ lab6/task.cpp
@@ -1,13 +1,11 @@
-#include <stdio.h>
 #include <iostream>
-#include <vector>
-#include <algorithm> 
 #include <cmath>
+#include <vector>
 
 #define MAX_ITERATION 1000000
 #define ACCURACY 0.0000001
 
-void init_matrix(std::vector<double>& matrix, int n) {
+void init_matrix(double* matrix, int n) {
     matrix[0] = 10.0;
     matrix[n - 1] = 20.0;
     matrix[(n - 1) * n] = 20.0;
@@ -17,14 +15,14 @@ void init_matrix(std::vector<double>& matrix, int n) {
         matrix[j] = matrix[0] + (matrix[n - 1] - matrix[0]) * (j / static_cast<double>(n - 1));
         matrix[(n - 1) * n + j] = matrix[(n - 1) * n] + (matrix[(n - 1) * n + (n - 1)] - matrix[(n - 1) * n]) * (j / static_cast<double>(n - 1));
         matrix[j * n] = matrix[0] + (matrix[(n - 1) * n] - matrix[0]) * (j / static_cast<double>(n - 1));
-        matrix[j * n + (n - 1)] = matrix[(n - 1)] + (matrix[(n - 1) * n + (n - 1)] - matrix[(n - 1) * n]) * (j / static_cast<double>(n - 1));
+        matrix[j * n + (n - 1)] = matrix[n - 1] + (matrix[(n - 1) * n + (n - 1)] - matrix[(n - 1) * n]) * (j / static_cast<double>(n - 1));
     }
 }
 
 int main() {
     int n = 20;
-    std::vector<double> matrix(n * n, 0.0);
-    std::vector<double> matrix_new(n * n, 0.0);
+    double* matrix = new double[n * n];
+    double* matrix_new = new double[n * n];
 
     init_matrix(matrix, n);
     init_matrix(matrix_new, n);
@@ -32,14 +30,12 @@ int main() {
     double err = 1.0;
     int iter = 0;
 
-    // Копируем данные на устройство
     #pragma acc enter data copyin(matrix[0:n*n], matrix_new[0:n*n], err, iter, n)
 
     while (err > ACCURACY && iter < MAX_ITERATION) {
         err = 0.0;
         
-        // Вычисляем новые значения и ошибку
-        #pragma acc parallel loop present(matrix, matrix_new) independent collapse(2) reduction(max:err)
+        #pragma acc parallel loop present(matrix, matrix_new) collapse(2) reduction(max:err)
         for (int i = 1; i < n - 1; i++) {
             for (int j = 1; j < n - 1; j++) {
                 matrix_new[i * n + j] = 0.25 * (
@@ -52,8 +48,7 @@ int main() {
             }
         }
 
-        // Копируем новые значения обратно в matrix
-        #pragma acc parallel loop present(matrix, matrix_new) independent collapse(2)
+        #pragma acc parallel loop present(matrix, matrix_new) collapse(2)
         for (int i = 1; i < n - 1; i++) {
             for (int j = 1; j < n - 1; j++) {
                 matrix[i * n + j] = matrix_new[i * n + j];
@@ -63,7 +58,6 @@ int main() {
         iter++;
     }
 
-    // Копируем данные обратно на хост
     #pragma acc exit data copyout(matrix[0:n*n])
 
     // Вывод матрицы
@@ -73,6 +67,9 @@ int main() {
         }
         std::cout << std::endl;
     }
+
+    delete[] matrix;
+    delete[] matrix_new;
 
     return 0;
 }
