8e9ddb70975bcad76ebf4bf57346c316ce6d9ff0
svetlana <svetlana.r.fisher@gmail.com>
2025-05-01 18:00:22 +0700

    fix

--- lab6/task.cpp
+++ lab6/task.cpp
@@ -39,16 +39,21 @@ int main() {
             {
                 err = 0.0;
 
-                #pragma acc loop collapse(2) reduction(max:err)
+                #pragma acc parallel loop collapse(2) reduction(max:err) present(matrix, matrix_new)
                 for (int i = 1; i < n - 1; i++) {
                     for (int j = 1; j < n - 1; j++) {
-                        matrix_new[i * n + j] = 0.25 * (matrix[(i + 1) * n + j] + matrix[i * n + j + 1] + 
-                                            matrix[i * n + j - 1] + matrix[(i - 1) * n + j]);
+                        matrix_new[i * n + j] = 0.25 * (
+                            matrix[(i + 1) * n + j] + 
+                            matrix[i * n + j + 1] + 
+                            matrix[i * n + j - 1] + 
+                            matrix[(i - 1) * n + j]
+                        );
                         err = fmax(err, fabs(matrix_new[i * n + j] - matrix[i * n + j]));
                     }
                 }
 
                 #pragma acc loop collapse(2)
+                #pragma acc parallel loop collapse(2) present(matrix, matrix_new)
                 for (int i = 1; i < n - 1; i++) {
                     for (int j = 1; j < n - 1; j++) {
                         matrix[i * n + j] = matrix_new[i * n + j];
