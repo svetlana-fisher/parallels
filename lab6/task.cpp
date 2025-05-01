db7d7d708789a45d919d8502678fbd13e5d066b5
svetlana <svetlana.r.fisher@gmail.com>
2025-05-01 18:50:18 +0700

    fix

--- lab6/task.cpp
+++ lab6/task.cpp
@@ -52,7 +52,6 @@ int main() {
                     }
                 }
 
-                #pragma acc loop collapse(2)
                 #pragma acc parallel loop collapse(2) present(matrix, matrix_new)
                 for (int i = 1; i < n - 1; i++) {
                     for (int j = 1; j < n - 1; j++) {
