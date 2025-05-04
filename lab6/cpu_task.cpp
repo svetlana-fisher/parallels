4f36a6fdefe0e24e5c06c8d3dec92e3b4f070553
svetlana <svetlana.r.fisher@gmail.com>
2025-05-04 19:39:13 +0700

    normal task

--- lab6/cpu_task.cpp
+++ lab6/cpu_task.cpp
@@ -3,26 +3,28 @@
 #include <vector>
 #include <algorithm> 
 #include <cmath>
+#include <fstream>
 #include <boost/program_options.hpp>
 
 namespace po = boost::program_options;
 
-void init_matrix(double* matrix, int n) {
-    matrix[0] = 10.0;
-    matrix[n - 1] = 20.0;
-    matrix[(n - 1) * n] = 20.0;
-    matrix[(n - 1) * n + (n - 1)] = 30.0;
 
-    for (int j = 1; j < n - 1; ++j) {
-        matrix[j] = matrix[0] + (matrix[n - 1] - matrix[0]) * (j / static_cast<double>(n - 1));
-        matrix[(n - 1) * n + j] = matrix[(n - 1) * n] + (matrix[(n - 1) * n + (n - 1)] - matrix[(n - 1) * n]) * (j / static_cast<double>(n - 1));
-        matrix[j * n] = matrix[0] + (matrix[(n - 1) * n] - matrix[0]) * (j / static_cast<double>(n - 1));
-        matrix[j * n + (n - 1)] = matrix[n - 1] + (matrix[(n - 1) * n + (n - 1)] - matrix[n - 1]) * (j / static_cast<double>(n - 1));
+void init_matrix(double* matrix, int n) {
+    matrix[n+1] = 10.0;
+    matrix[n - 1 + n - 1] = 20.0;
+    matrix[(n - 1) * (n - 1)] = 20.0;
+    matrix[(n - 1) * (n - 1) + (n - 1) - 2] = 30.0;
+
+    for (int j = 2; j < n - 2; j++) {
+        matrix[n + j] = matrix[n + 1] + (matrix[n - 1 + n - 1] - matrix[n + 1]) * (static_cast<double>(j-1) / static_cast<double>(n - 3));
+        matrix[(n - 1) * (n - 1) + j - 1] = matrix[(n - 1) * (n - 1)] + (matrix[(n - 1) * (n - 1) + (n - 1) - 2] - matrix[(n - 1) * (n - 1)]) * (static_cast<double>(j-1) / static_cast<double>(n - 3));
+        matrix[j * n + 1] = matrix[n + 1] + (matrix[(n - 1) * (n - 1)] - matrix[n + 1]) * (static_cast<double>(j-1) / static_cast<double>(n - 3));
+        matrix[j * n + (n - 1) - 1] = matrix[n - 1 + n - 1] + (matrix[(n - 1) * (n - 1) + (n - 1) - 2] - matrix[n - 1 + n - 1]) * (static_cast<double>(j-1) / static_cast<double>(n - 3));
     }
 }
 
+
 int main(int argc, char** argv) {
-    // Параметры по умолчанию
     int n = 20;
     double accuracy = 1e-6;
     int max_iteration = 1000000;
@@ -44,9 +46,8 @@ int main(int argc, char** argv) {
         return 0;
     }
 
-
-    double* matrix = new double[n * n];
-    double* matrix_new = new double[n * n];
+    double* matrix = new double[(n + 2) * (n + 2)];
+    double* matrix_new = new double[(n + 2) * (n + 2)];
 
     init_matrix(matrix, n);
     init_matrix(matrix_new, n);
@@ -54,9 +55,11 @@ int main(int argc, char** argv) {
     double err = 1.0;
     int iter = 0;
 
+
     while (err > accuracy && iter < max_iteration) {
         err = 0.0;
 
+        #pragma acc parallel loop collapse(2) reduction(max:err)
         for (int i = 1; i < n - 1; i++) {
             for (int j = 1; j < n - 1; j++) {
                 matrix_new[i * n + j] = 0.25 * (
@@ -69,6 +72,7 @@ int main(int argc, char** argv) {
             }
         }
 
+        #pragma acc parallel loop collapse(2)
         for (int i = 1; i < n - 1; i++) {
             for (int j = 1; j < n - 1; j++) {
                 matrix[i * n + j] = matrix_new[i * n + j];
@@ -78,12 +82,16 @@ int main(int argc, char** argv) {
     }
 
     // Вывод матрицы
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            std::cout << matrix[i * n + j] << " ";
-        }
-        std::cout << std::endl;
-    }
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < n; ++j) {
+    //         std::cout << matrix[i * n + j] << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+
+    std::ofstream out_file("result.dat", std::ios::binary);
+    out_file.write(reinterpret_cast<const char*>(matrix), n * n * sizeof(double));
+    out_file.close();
 
     std::cout << "Iterations: " << iter << "\n";
     std::cout << "Final error: " << err << "\n";
